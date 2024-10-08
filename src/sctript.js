7a053e532f23f2c1f4f55e7fd72741e0e05f4f14
ptsfdtz <pitousanfadetuzi@gmail.com>
2024-10-08 08:58:18 +0800

    init

---
+++ src/sctript.js
@@ -0,0 +1,45 @@
+function loadPage(url) {
+    const contentDiv = document.getElementById("content");
+    let page = "";
+
+    switch (url) {
+        case "/":
+        case "#/":
+            page = "pages/home/index.html";
+            break;
+        case "#/pageOne":
+            page = "pages/pageOne/index.html";
+            break;
+        case "#/pageTwo":
+            page = "pages/pageTwo/index.html";
+            break;
+        default:
+            page = "pages/404.html";
+            break;
+    }
+
+    fetch(page)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Page not found');
+            }
+            return response.text();
+        })
+        .then(html => {
+            contentDiv.innerHTML = html;
+        })
+}
+
+function navigateTo(url) {
+    window.location.hash = url;
+    loadPage(window.location.hash);
+}
+
+window.onload = function () {
+    const currentHash = window.location.hash || "#/";
+    loadPage(currentHash);
+};
+
+window.addEventListener('hashchange', function () {
+    loadPage(window.location.hash);
+});
